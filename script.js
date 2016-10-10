c54a0fcf1faf1d19d6eba91c7b7b41c527e91ece
Ludovic Meurillon <ludovic.meurillon@gmail.com>
2016-10-10 22:57:17 +0200

    demos about storage, network and sources panels

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var basicDebug = function(node){
     node.innerHTML = "coucou " + node.innerHTML;
 }
 
-var sayHello = function(index){
-    console.log("Hello dev day", i);
+var sayHello = function(source, index){
+    console.log("Hello dev day", index, "from", source);
 }
 
-var advancedDebug = function(){
+var advancedDebug = function(node){
     for(var i=0; i<10; i++){
-        sayHello(i);
+        sayHello(node, i);
     }
 }
 
@@ -72,4 +72,58 @@ var nested = function(){
     base(10);
     base(56);
     console.groupEnd("Fonction globale");
+}
+
+
+
+////////////////////////////////
+
+
+var cookieTest = function(){
+    document.cookie = "devday-prez=ChromeAsYourIDE";
+    document.cookie = "devday-slide=Application";
+    document.cookie = "devday-speaker=Ludovic Meurillon";
+    document.cookie = "devday-time="+new Date().getTime();
+    console.debug(document.cookie);
+}
+
+
+var storageTest = function(){
+    var key = "devday";
+    localforage.config({
+        driver: localforage.INDEXEDDB,
+        name: "DevDayStore",
+        storeName: "values"
+    });
+
+    var values = [];
+    values.push({type:"local", valeur: localStorage[key]});
+    values.push({type:"session", valeur: sessionStorage[key]});
+
+    localforage.getItem(key)
+        .then(function(value){
+            values.push({type:"indexedDB", valeur: value});
+            return values;
+        })
+        .catch(function(error){
+            values.push({type:"indexedDB", valeur: error});
+            return values;
+        })
+        .then(function(values){
+            console.table(values);
+        });
+
+    //Update stores
+    localStorage[key] = new Date().getTime();
+    sessionStorage[key] = new Date().getTime();
+
+    localforage.setItem(key, new Date().getTime());
+}
+
+
+var loadImages = function(node){
+    var parentNode = $('.c-images');
+    for(var i=1; i<16; i++){
+        parentNode.innerHTML = parentNode.innerHTML+'<img src="images/orange'+i+'.png" class="c-media"/>'
+    }
 }
