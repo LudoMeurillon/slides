ca2089a1c5e08114b51b3ab8b15e9b704f04275d
MEURILLON Ludovic IMT/OLPS <DMQJ0354@orange.com>
2016-10-10 20:58:24 +0200

    explode css/js from html file to dzslides

--- script.js
+++ script.js
@@ -1,7 +1,75 @@
-console.log("Hello Dev Day");
-
-var remplir = function(node){
+var basicDebug = function(node){
     node.innerHTML = "coucou " + node.innerHTML;
-    console.debug("Un truc tellement fou que ca se loggue dans la console");
-    console.warn("Un alerte peu aussi avoir une stacktrace")
+}
+
+var sayHello = function(index){
+    console.log("Hello dev day", i);
+}
+
+var advancedDebug = function(){
+    for(var i=0; i<10; i++){
+        sayHello(i);
+    }
+}
+
+var levels = function(){
+    debugger;
+    console.log("niveau de base");
+    debugger;
+    console.info("niveau info avec une jolie icône");
+    debugger;
+    console.debug("En mode debug avec des logs");
+    debugger;
+    console.warn("Une alerte jaune");
+    debugger;
+    console.error("Une erreur inconnue");
+    debugger;
+    console.trace("Une trace à la main");
+}
+
+var traitementLong = function(){
+    var date = new Date().getTime();
+    console.time(date);
+
+    var sleep = Math.floor(Math.random() * 1000);
+    console.log("une fonction qui dure environ ", sleep, "ms");
+
+    setTimeout(function(){
+        console.timeEnd(date);
+    }, sleep);
+}
+
+var table = function(){
+    var values = [
+        {
+            nom: "Warren",
+            prenom: "Marquis",
+            role: "major"
+        },
+        {
+            nom: "Domergue",
+            prenom: "Daisy",
+            role: "prisoner"
+        },
+        {
+            nom: "Ruth",
+            prenom: "John",
+            role: "hangman"
+        }
+    ]
+
+    console.table(values);
+}
+
+var base = function(index){
+    console.group("base"+index);
+    console.log("un truc basique");
+    console.groupEnd("base"+index);
+}
+
+var nested = function(){
+    console.group("Fonction globale");
+    base(10);
+    base(56);
+    console.groupEnd("Fonction globale");
 }
